6278b2423421fabbcdd332896e6720a39fd86e79
DESKTOP-9G8557Q\Lnhie <HieuLNGBH19006@fpt.com.vn>
2024-07-09 14:52:26 +0700

    hieuln update source

--- server/weather-forecast-microservices/Services/Data/Data/Service/WeatherConsumerService.cs
+++ server/weather-forecast-microservices/Services/Data/Data/Service/WeatherConsumerService.cs
@@ -24,7 +24,7 @@ namespace Data.Service
 		public async Task GetCurrentWeatherData()
 		{
 			_model.QueueDeclare(currentWeatherQueue, exclusive: false);
-			var consumer = new AsyncEventingBasicConsumer(_model);
+			var consumer = new AsynccConsumer(_model);
 
 			consumer.Received += async (model, ea) =>
 			{
